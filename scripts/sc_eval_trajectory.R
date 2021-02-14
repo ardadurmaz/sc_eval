0e8d93372a95ce358b4731a9c5ca16c6607dc8b3
Arda Durmaz <12184397+ardadurmaz@users.noreply.github.com>
2021-02-14 10:04:23 -0500

    Add files via upload

---
+++ scripts/sc_eval_trajectory.R
@@ -0,0 +1,51 @@
+library(Matrix)
+
+
+suppressMessages(source('~/sc_eval/scripts/sc_utils.R'))
+suppressMessages(source('~/sc_eval/scripts/sc_functions.R'))
+
+setwd('~/sc_eval/data/')
+set.seed(42)
+CombinedData <- readRDS('PancreaticMaturation_A.rds')
+
+## Run ##
+for(i in 1:length(CombinedData)){
+  
+  local_sceval <- CombinedData[[i]]
+  log_file <- paste0(local_sceval@title, '.log')
+  wd <- sprintf('~/sc_eval/data/%s', local_sceval@title)
+  setwd(wd)
+  
+  ## Impute ##
+  counts(local_sceval) <- list('scimpute' = readRDS(sprintf('%s_ScImpute.rds', local_sceval@title)))
+  
+  ## Normalize ##
+  norm(local_sceval) <- list('raw_deconv' = readRDS(sprintf('%s_RawDeconv.rds', local_sceval@title)))
+  norm(local_sceval) <- list('raw_sctransform' = readRDS(sprintf('%s_RawScTransform.rds', local_sceval@title)))
+  norm(local_sceval) <- list('raw_dca_nb' = readRDS(sprintf('%s_RawDCA_NB.rds', local_sceval@title)))
+  norm(local_sceval) <- list('raw_dca_zinb' = readRDS(sprintf('%s_RawDCA_ZINB.rds', local_sceval@title)))
+  norm(local_sceval) <- list('scimpute_deconv' = readRDS(sprintf('%s_ScImputeDeconv.rds', local_sceval@title)))
+  norm(local_sceval) <- list('scimpute_sctransform' = readRDS(sprintf('%s_ScImputeScTransform.rds', local_sceval@title)))
+  norm(local_sceval) <- list('drimpute_deconv' = readRDS(sprintf('%s_DrImputeDeconv.rds', local_sceval@title)))
+  norm(local_sceval) <- list('drimpute_sctransform' = readRDS(sprintf('%s_DrImputeScTransform.rds', local_sceval@title)))
+  
+  ## Dimension Reduction ##
+  for(d_type in c('raw', 'scimpute', 'drimpute')){
+    for(n_type in c('deconv', 'sctransform', 'dca_nb', 'dca_zinb')){
+      for(r_type in c('umap', 'tsne', 'vae', 'dm', 'paga_umap')){
+        if((d_type == 'scimpute' || d_type == 'drimpute') && (n_type == 'dca_nb' || n_type == 'dca_zinb'))
+          next
+        
+        tag <- sprintf('%s_%s_%s', d_type, n_type, r_type)
+        redDim(local_sceval) <- setNames(list(readRDS(sprintf('%s_%s.rds', local_sceval@title, tag))), tag)
+      }
+    }
+  }
+
+  ## Trajectory Inference ##
+  cat(sprintf('SC_EVAL [%s]: Running trajectory inference...\n', date()), file = log_file, append = TRUE)
+  local_sceval <- sc_traj(local_sceval, method = 'slingshot')
+  local_sceval <- sc_traj(local_sceval, method = 'monocle')
+  local_sceval <- sc_traj(local_sceval, method = 'OT')
+}
+cat(sprintf('trajectory inference done..'))
